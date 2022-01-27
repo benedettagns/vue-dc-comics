b32f2e46480886c88fa6b5bb901f8672dbdfb3a4
benedettagns <benedetta.genise01@gmail.com>
2022-01-27 16:26:30 +0100

    header

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header-container/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderContainer from './components/HeaderContainer.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HeaderContainer,
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/HeaderContainer.vue
@@ -0,0 +1,61 @@
+<template>
+    <header>
+        <div class="logo-box">
+            <img src="../assets/img/dc-logo.png" alt="">
+        </div>
+
+        <ul>
+            <li>characters</li>
+            <li>comics</li>
+            <li>movies</li>
+            <li>tv</li>
+            <li>games</li>
+            <li>collectibles</li>
+            <li>video</li>
+            <li>fans</li>
+            <li>news</li>
+            <li>shop</li>
+        </ul>    
+    </header>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+header{
+    height: 100px;
+    width: 100%;
+    display: flex;
+    justify-content: space-around;
+    
+    .logo-box {
+        margin-top: 15px ;
+
+        img {
+            width: 80px;
+        }
+    }
+    
+    ul {
+        list-style: none;
+        display: flex;
+        margin-top: 40px;
+        
+        li {
+            padding: 10px;
+            text-transform: uppercase;
+            cursor: pointer;
+
+            &:hover {
+                border-bottom: 3px blue solid;
+            }
+        }
+    }
+    
+}
+
+</style>
