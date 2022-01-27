5ed03b2442050f8240858c52812dc8448f0850b6
benedettagns <benedetta.genise01@gmail.com>
2022-01-27 17:41:31 +0100

    navbar

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
   <div id="app">
     <header-container/>
     <jumbotron-box/>
+    <navbar-container/>
   </div>
 </template>
 
 <script>
 import HeaderContainer from './components/HeaderContainer.vue'
 import JumbotronBox from './components/JumbotronBox.vue'
+import NavbarContainer from './components/NavbarContainer.vue'
 
 export default {
   name: 'App',
   components: {
     HeaderContainer,
     JumbotronBox,
+    NavbarContainer,
   }
 }
 </script>
---
+++ src/components/NavbarContainer.vue
@@ -0,0 +1,51 @@
+<template>
+    <footer>
+        <div class="navbar">
+            
+            <img src="../assets/img/buy-comics-digital-comics.png" alt="">
+            <span>digital comics</span>
+        
+            <img src="../assets/img/buy-comics-merchandise.png" alt="">
+            <span>dc merchandise</span>
+        
+            <img src="../assets/img/buy-comics-subscriptions.png" alt="">
+            <span>subscriptions</span>
+        
+            <img src="../assets/img/buy-comics-shop-locator.png" alt="">
+            <span>comic shop locator</span>
+        
+            <img src="../assets/img/buy-dc-power-visa.svg" alt="">
+            <span>dc power visa</span>
+        
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+.navbar {
+    height: 100px;
+    background-color: blue;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    
+    img {
+        width: 40px;
+    }
+
+    span {
+        text-transform: uppercase;
+        color: white;
+        font-size: 12px ;
+        margin-right: 70px;
+        padding-left: 10px ;
+    }
+    
+}
+</style>
