e5bad390a93eb9007dcb91e0c3a3cc09b4cf387b
benedettagns <benedetta.genise01@gmail.com>
2022-01-27 16:44:59 +0100

    jumbotron

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,23 @@
 <template>
   <div id="app">
     <header-container/>
+    <jumbotron-box/>
   </div>
 </template>
 
 <script>
 import HeaderContainer from './components/HeaderContainer.vue'
+import JumbotronBox from './components/JumbotronBox.vue'
 
 export default {
   name: 'App',
   components: {
     HeaderContainer,
+    JumbotronBox,
   }
 }
 </script>
 
 <style lang="scss">
-
+@import './style/main.scss'
 </style>
---
+++ src/components/JumbotronBox.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+    div {
+        background: url('../assets/img/jumbotron.jpg');
+        height: 300px;
+        background-size: cover;
+    }
+</style>
