c8001cacff97f0b9a6475d276a2127c9c3343d3e
benedettagns <benedetta.genise01@gmail.com>
2022-01-27 22:22:41 +0100

    footer

---
+++ src/components/FooterContainer.vue
@@ -0,0 +1,132 @@
+<template>
+    <footer>
+        <div class="footer-box">
+            <div>
+                <p>DC COMIC</p>
+                <ul>
+                    <li>Characters</li>
+                    <li>Comics</li>
+                    <li>Movies</li>
+                    <li>TV</li>
+                    <li>Games</li>
+                    <li>Videos</li>
+                    <li>News</li>
+                </ul>
+
+                <p>SHOP</p>
+                <ul>
+                    <li>Shop DC</li>
+                    <li>Shop DC Collectibles</li>
+                </ul>
+            </div>
+            
+            <div>
+                <p>DC</p>
+                <ul>
+                    <li>Terms Of Use</li>
+                    <li>Privacy Policy(New)</li>
+                    <li>Ad Choises</li>
+                    <li>Advertising</li>
+                    <li>Jobs</li>
+                    <li>Subscriptions</li>
+                    <li>Talent Workshops</li>
+                    <li>CPSC Certificates</li>
+                    <li>Ratings</li>
+                    <li>Shop Help</li>
+                    <li>Contact Us</li>
+                </ul>
+            </div>
+            
+            <div>
+                <p>SITES</p>
+                <ul>
+                    <li>DC</li>
+                    <li>MAD Magazine</li>
+                    <li>DC Kids</li>
+                    <li>DC Universe </li>
+                    <li>DC Power Visa</li>
+                </ul>
+            </div>   
+        </div>
+        
+        <div class="follow">
+            <div class="button-box">
+                <button>SING-UP NOW!</button>
+            </div>
+            
+            <div class="social">
+                <span>FOLLOW US</span>
+                <img src="../assets/img/footer-facebook.png" alt="">
+                <img src="../assets/img/footer-twitter.png" alt="">
+                <img src="../assets/img/footer-youtube.png" alt="">
+                <img src="../assets/img/footer-pinterest.png" alt="">
+                <img src="../assets/img/footer-periscope.png" alt="">    
+            </div>
+        </div>
+
+    </footer>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+footer {
+    background: url('../assets/img/footer-bg.jpg');
+    background-position: cover;
+    
+    .footer-box {
+        display: flex;
+        flex-wrap: wrap;
+        padding: 50px;
+        
+        p {
+            color: white;   
+            padding-left: 30px; 
+            font-size: 20px;
+        }
+
+        ul {
+            list-style: none;
+            padding-left: 30px;
+            padding-bottom: 20px;
+
+            li {
+                color: white;
+            }
+        }
+    }
+    .follow {
+        height: 100px;
+        background-color: rgb(43, 42, 42);
+        display:flex;
+        justify-content: space-between;
+        align-items: center;
+
+        .button-box {
+            margin: 40px;
+
+            button {
+                background-color: grey;
+                border: blue 3px solid;
+                height: 40px;
+                width: 150px;
+            }
+        }
+
+        .social {
+            margin: 30px;
+
+            span {
+                padding-right: 40px;
+                font-size: 20px;
+                color: blue;  
+            }
+   
+        }
+    }
+}
+</style>
